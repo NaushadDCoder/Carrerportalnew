d44e0866d32496317a765ade8bbb2ee8cb180fd1
Naushad <Naushad@Naushad-PC>
2014-06-16 20:43:02 +0800

    This is from neha

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -7,7 +7,7 @@ $this->pageTitle=Yii::app()->name;
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
 <p>Congratulations! You have successfully created your Yii application.</p>
-
+<?php echo "this is testing" ; ?>
 <p>You may change the content of this page by modifying the following two files:</p>
 <ul>
 	<li>View file: <code><?php echo __FILE__; ?></code></li>
